f0674a75c51f53c7912f36cbb4654895d4e28e2c
Allen <s96016641@gmail.com>
2025-08-13 11:11:55 +0800

    init

---
+++ AppHost/Program.cs
@@ -0,0 +1,11 @@
+﻿var builder = DistributedApplication.CreateBuilder(args);
+
+var keycloak = builder
+    .AddKeycloakContainer("keycloak")
+    .WithDataVolume();
+
+var realm = keycloak.AddRealm("Test");
+
+builder.AddProject<Projects.Api>("api").WithReference(keycloak).WithReference(realm);
+
+builder.Build().Run();
